6a3102b77fb99ea89f9b8a89c27585547ffc2d10
Sang Uk Jung <83222288+JSU0318@users.noreply.github.com>
2023-05-12 17:51:05 +0900

    TSPrac4

--- app.ts
+++ app.ts
@@ -2,10 +2,22 @@ const person: {
   name: string;
   age: number;
   hobby: string[];
+  role: [number, string];
 } = {
   name: "Maximilian",
   age: 30,
   hobby: ["Sport", "Cooking"],
+  role: [2, "author"],
 };
+// person.role.push("admin");
+// person.role[1] = 10;
+person.role = [0, "stri"];
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
 
 console.log(person.name);
+
+for (const hobbies of person.hobby) {
+  console.log(hobbies.toUpperCase());
+}
